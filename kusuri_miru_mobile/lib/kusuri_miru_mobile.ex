0bdc4cc4082962938eaff41db9088aa8fe63f30e
negitoro001 <stnjllx7157@mc.pikara.ne.jp>
2023-10-10 11:17:23 +0900

    起動画面まで遷移した

--- kusuri_miru_mobile/lib/kusuri_miru_mobile.ex
+++ kusuri_miru_mobile/lib/kusuri_miru_mobile.ex
@@ -6,4 +6,48 @@ defmodule KusuriMiruMobile do
   Contexts are also responsible for managing your data, regardless
   if it comes from the database, an external API or others.
   """
+  use Application
+
+  def config_dir() do
+    Path.join([Desktop.OS.home(), ".config", "kusuri_miru_mobile"])
+  end
+
+  @app Mix.Project.config()[:app]
+  def start(:normal, []) do
+    File.mkdir_p!(config_dir())
+    # DBの設定
+    #Application.put_env(:kusuri_miru_mobile, KusuriMiruMobile.Repo,
+    #  database: Path.join(config_dir())
+    #)
+
+    :session = :ets.new(:session, [:named_table, :public, read_concurrency: true])
+
+    children = [
+      #KusuriMiruMobile.Repo,
+      #{Phoenix.PubSub, name: KusuriMiruMobile.PubSub},
+      KusuriMiruMobileWeb.Endpoint
+    ]
+
+    opts = [strategy: :one_for_one, name: KusuriMiruMobile.Supervisor]
+
+    {:ok, sup} = Supervisor.start_link(children, opts)
+    port = :ranch.get_port(KusuriMiruMobileWeb.Endpoint.HTTP)
+
+    {:ok, _} =
+      Supervisor.start_child(sup, {
+        Desktop.Window,
+        [
+          app: @app,
+          id: KusuriMiruMobileWindow,
+          title: "Kusuri_miru_mobile",
+          size: {400, 800},
+          url: "http://localhost:#{port}"
+        ]
+      })
+  end
+
+  def config_change(changed, _new, removed) do
+    KusuriMiruMobile.Endpoint.config_change(changed, removed)
+    :ok
+  end
 end
