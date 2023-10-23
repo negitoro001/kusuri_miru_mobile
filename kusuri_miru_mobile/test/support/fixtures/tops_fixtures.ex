745ea48b18f15ef429ad0d6d9b4f174357329e5a
negitoro001 <stnjllx7157@mc.pikara.ne.jp>
2023-10-23 16:16:11 +0900

    testを途中まで作成

--- kusuri_miru_mobile/test/support/fixtures/tops_fixtures.ex
+++ kusuri_miru_mobile/test/support/fixtures/tops_fixtures.ex
@@ -7,17 +7,8 @@ defmodule KusuriMiruMobile.TopsFixtures do
   @doc """
   Generate a top.
   """
-  def top_fixture(attrs \\ %{}) do
-    {:ok, top} =
-      attrs
-      |> Enum.into(%{
-        icon: "some icon",
-        id: "some id",
-        name: "some name",
-        tags: ["option1", "option2"]
-      })
-      |> KusuriMiruMobile.Tops.create_top()
-
-    top
+  def top_fixture(_attrs \\ %{}) do
+    attrs = KusuriMiruMobile.Tops.list_tops()
+    {:ok, attrs}
   end
 end
