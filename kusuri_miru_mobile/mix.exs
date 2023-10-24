005a768f911597fadce4e713c01972fb86e533df
negitoro001 <stnjllx7157@mc.pikara.ne.jp>
2023-10-24 17:06:19 +0900

    revert disable deps

--- kusuri_miru_mobile/mix.exs
+++ kusuri_miru_mobile/mix.exs
@@ -62,8 +62,8 @@ defmodule KusuriMiruMobile.MixProject do
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
-      #{:desktop, "~> 1.5"},
-      #{:wx, "~>1.1", hex: :bridge, targets: [:android, :ios, :desktop]},
+      {:desktop, "~> 1.5"},
+      {:wx, "~>1.1", hex: :bridge, targets: [:android, :ios, :desktop]},
       {:req, "~>0.4.4"}
     ]
   end
