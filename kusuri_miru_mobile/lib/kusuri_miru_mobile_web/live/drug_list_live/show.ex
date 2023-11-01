8b80326a9a073b4db9e44ff7df1b881559da6dd7
negitoro001 <stnjllx7157@mc.pikara.ne.jp>
2023-11-01 15:04:33 +0900

    ソート完成

--- kusuri_miru_mobile/lib/kusuri_miru_mobile_web/live/drug_list_live/show.ex
+++ kusuri_miru_mobile/lib/kusuri_miru_mobile_web/live/drug_list_live/show.ex
@@ -18,9 +18,39 @@ defmodule KusuriMiruMobileWeb.DrugListLive.Show do
 
   @impl true
   def handle_event("sort_option", params, socket) do
-    sort = params["name"]
-    socket = assign(socket, :drug_list, socket.assigns.drug_list |> Enum.sort_by(& &1.sort, :desc))
-    {:noreply, socket}
+    sort = String.to_atom(params["name"])
+
+    case sort do
+      :created_at ->
+        socket =
+          assign(
+            socket,
+            :drug_list,
+            socket.assigns.drug_list |> Enum.sort_by(& &1.created_at, :desc)
+          )
+
+        {:noreply, socket}
+
+      :avg_rating ->
+        socket =
+          assign(
+            socket,
+            :drug_list,
+            socket.assigns.drug_list |> Enum.sort_by(& &1.avg_rating, :desc)
+          )
+
+        {:noreply, socket}
+
+      :count_ratings ->
+        socket =
+          assign(
+            socket,
+            :drug_list,
+            socket.assigns.drug_list |> Enum.sort_by(& &1.count_ratings, :desc)
+          )
+
+        {:noreply, socket}
+    end
   end
 
   defp page_title(:show), do: "Show Drug list"
